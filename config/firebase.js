a7871107b861cc2682724527292cb248cac2e954
Himanshu72060 <himanshusharma00898@gmail.com>
2026-09-24 17:52:00 +0530

    fix admin route 404

--- config/firebase.js
+++ config/firebase.js
@@ -17,15 +17,19 @@
 
 // module.exports =
 //   admin;
+
+
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: serviceAccount.project_id,
+      clientEmail: serviceAccount.client_email,
+      privateKey: serviceAccount.private_key.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
